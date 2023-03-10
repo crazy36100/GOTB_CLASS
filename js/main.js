884f4349573834f22f8958676608abe1ea898a46
crazy36100 <127503879+crazy36100@users.noreply.github.com>
2023-03-10 19:40:44 +0630

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ btn.addEventListener("click", () => {
     window.open("https://gotb.netlify.app/home.html")
   }
 });
+
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
